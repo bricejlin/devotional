9163195f786064d5f321a80f16c9ab0af6c3130b
bricejlin <bricejlin@gmail.com>
2013-08-26 19:38:25 -0400

    changed form css

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -10,7 +10,6 @@ class ContactController < ApplicationController
   		NotificationsMailer.contact_us(@message).deliver
   		redirect_to root_url, notice: "Message successfully sent!"
   	else
-  		flash.now.alert = "Please fill all fields."
   		render 'new'
   	end
   end
